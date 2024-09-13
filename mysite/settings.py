555c8d4f2755a65e595d37ba712e6b9b6eccc7b4
karintena <mariakarineces@gmail.com>
2024-09-13 14:31:43 -0300

    mensagem

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*0z35f5%!zylv6ym=4tff65v8i@-tno2e2s+2hifazl%fl2p#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1' , '.pythonanywhere.com']
 
 
 # Application definition
